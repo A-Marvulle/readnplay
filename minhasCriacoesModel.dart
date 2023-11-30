c5e810689be5c3403de2857116024c3f1b8d0e61
Alfredo <107883417+A-Marvulle@users.noreply.github.com>
2023-11-30 11:14:05 -0300

    Create minhasCriacoesModel.dart

---
+++ minhasCriacoesModel.dart
@@ -0,0 +1,30 @@
+import '/auth/firebase_auth/auth_util.dart';
+import '/backend/backend.dart';
+import '/components/quiz_sets_new_widget.dart';
+import '/flutter_flow/flutter_flow_theme.dart';
+import '/flutter_flow/flutter_flow_util.dart';
+import '/flutter_flow/flutter_flow_widgets.dart';
+import 'minhas_criacoes_widget.dart' show MinhasCriacoesWidget;
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
+
+class MinhasCriacoesModel extends FlutterFlowModel<MinhasCriacoesWidget> {
+  ///  State fields for stateful widgets in this page.
+
+  final unfocusNode = FocusNode();
+
+  /// Initialization and disposal methods.
+
+  void initState(BuildContext context) {}
+
+  void dispose() {
+    unfocusNode.dispose();
+  }
+
+  /// Action blocks are added here.
+
+  /// Additional helper methods are added here.
+}
