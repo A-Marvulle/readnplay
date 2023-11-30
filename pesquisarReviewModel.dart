da677f254df6a6c2c0fd09758988f9b01caf3bfa
Alfredo <107883417+A-Marvulle@users.noreply.github.com>
2023-11-30 11:13:49 -0300

    Create pesquisarReviewModel.dart

---
+++ pesquisarReviewModel.dart
@@ -0,0 +1,40 @@
+import '/backend/backend.dart';
+import '/flutter_flow/flutter_flow_autocomplete_options_list.dart';
+import '/flutter_flow/flutter_flow_theme.dart';
+import '/flutter_flow/flutter_flow_util.dart';
+import '/flutter_flow/flutter_flow_widgets.dart';
+import 'pesquisar_review_widget.dart' show PesquisarReviewWidget;
+import 'package:easy_debounce/easy_debounce.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
+import 'package:flutter/services.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
+import 'package:text_search/text_search.dart';
+
+class PesquisarReviewModel extends FlutterFlowModel<PesquisarReviewWidget> {
+  ///  State fields for stateful widgets in this page.
+
+  final unfocusNode = FocusNode();
+  // State field(s) for TextField widget.
+  final textFieldKey = GlobalKey();
+  FocusNode? textFieldFocusNode;
+  TextEditingController? textController;
+  String? textFieldSelectedOption;
+  String? Function(BuildContext, String?)? textControllerValidator;
+  List<ReviewRecord> simpleSearchResults = [];
+
+  /// Initialization and disposal methods.
+
+  void initState(BuildContext context) {}
+
+  void dispose() {
+    unfocusNode.dispose();
+    textFieldFocusNode?.dispose();
+  }
+
+  /// Action blocks are added here.
+
+  /// Additional helper methods are added here.
+}
